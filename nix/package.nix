d8c09c891eef0d6396756a3835434976ce73677f
nanoyaki <hanakretzer@gmail.com>
2025-01-18 19:13:28 +0100

    chore: bump nix package version to 0.5.0

--- nix/package.nix
+++ nix/package.nix
@@ -5,11 +5,11 @@
 
 php84.buildComposerProject2 {
   pname = "discord-events-to-ics";
-  version = "0.4.1";
+  version = "0.5.0";
 
   src = lib.cleanSource ./..;
 
   composerNoPlugins = false;
   composerLock = ../composer.lock;
-  vendorHash = "sha256-b4fbGZXPDj5ywu+UUaIi1TmnhJcbcmm3TrszTWVuOh4=";
+  vendorHash = "sha256-RutLVUZ1YbypX0O1WwBJ731mKMvTW/qmws58qx0U44M=";
 }
