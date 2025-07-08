e8d5f7549701714e15960c724f1797854bd89de0
nanoyaki <hanakretzer@gmail.com>
2025-07-08 15:34:54 +0200

    feat: set meta.broken

--- nix/package.nix
+++ nix/package.nix
@@ -12,4 +12,6 @@ php84.buildComposerProject2 {
   composerNoPlugins = false;
   composerLock = ../composer.lock;
   vendorHash = "sha256-N6g/E/+5MF1AZo8qO/vqNXKIUJEKh0+2KedVvnu3LIk=";
+
+  meta.broken = true;
 }
